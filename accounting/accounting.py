7345e480cd72651376d71b1d13b16b49f71e40a8
Nikolett Uracs <loveninagold@gmail.com>
2016-11-29 15:03:48 +0100

    accounting 80% done

--- accounting/accounting.py
+++ accounting/accounting.py
@@ -10,6 +10,8 @@
 
 # importing everything you need
 import os
+import common
+import ui
 from importlib.machinery import SourceFileLoader
 current_file_path = os.path.dirname(os.path.abspath(__file__))
 # User interface module
@@ -26,8 +28,36 @@ common = SourceFileLoader("common", current_file_path + "/../common.py").load_mo
 #
 def start_module():
 
-    # you code
+    account_table = ["Adding",
+                     "Remove",
+                     "Update",
+                     "Highest profit",
+                     "Avg profit"]
 
+    table_accounting = []
+    with open("accounting/items.csv", "r") as my_file:
+        for lines in my_file:
+            table_accounting.append(lines.strip().split("\t"))
+
+    ui.print_menu("Accounting Manager", account_table, "Back to main menu")
+    inputs = ui.get_inputs(["Please enter a number: "], "")
+    option = inputs[0]
+    if option == "1":
+        show_table(table_accounting)
+    elif option == "2":
+        add(table_accounting)
+    elif option == "3":
+        remove(table_accounting)
+    elif option == "4":
+        update(table_accounting)
+    elif option == "5":
+        which_year_max(table_accounting)
+    elif option == "6":
+        avg_amount(table_accounting)
+    elif option == "0":
+        sys.exit(0)
+    else:
+        raise KeyError("There is no such option.")
     pass
 
 
@@ -35,8 +65,9 @@ def start_module():
 #
 # @table: list of lists
 def show_table(table):
+    print(table)
+    ui.print_table(table, ["ID", "Month", "Day", "Year", "In/out", "Amount"])
 
-    # your code
 
     pass
 
@@ -46,8 +77,10 @@ def show_table(table):
 # @table: list of lists
 def add(table):
 
-    # your code
+    new_id = ui.get.inputs(["Give an ID: ", "Give a month: ", "Give a day: ", "Give a year: ",
+                            "In or out(come): ", "Amount: " ], "Adding record")
 
+    table.append([new_id[0], new_id[1], new_id[2], new_id[3], new_id[4], new_id[5]])
     return table
 
 
@@ -57,7 +90,9 @@ def add(table):
 # @id_: string
 def remove(table, id_):
 
-    # your code
+    for i in range(len(table)):
+        if id_ in table[i]:
+            table.remove(table[i])
 
     return table
 
@@ -69,7 +104,15 @@ def remove(table, id_):
 # @id_: string
 def update(table, id_):
 
-    # your code
+    for i in range(len(table)):
+        if id_ in table[i]:
+            update_id = ui.get.inputs(["Give a month: ", "Give a day: ", "Give a year: ",
+                                    "In or out(come): ", "Amount: " ], "Adding record")
+            table[i][1] = update_id[0]
+            table[i][2] = update_id[1]
+            table[i][3] = update_id[2]
+            table[i][4] = update_id[3]
+            table[i][5] = update_id[4]
 
     return table
 
@@ -80,16 +123,34 @@ def update(table, id_):
 # the question: Which year has the highest profit? (profit=in-out)
 # return the answer (number)
 def which_year_max(table):
-
-    # your code
-
-    pass
-
+    profits = []
+    for i in range(len(table)):
+        profits.append(table[i][5])
+    max_profit = max(profits)
+    for i in range(len(table)):
+        if max_profit == table[i][5]:
+            year = table[i][3]
+    return int(year)
 
 # the question: What is the average (per item) profit in a given year? [(profit)/(items count) ]
 # return the answer (number)
 def avg_amount(table, year):
-
-    # your code
-
-    pass
+    in_list = []
+    out_list = []
+    items = 0
+    for i in table:
+        if int(i[3]) == int(year):
+            items += 1
+            if str(i[4]) == "in":
+                in_list.append(int(i[5]))
+            elif str(i[4]) == "out":
+                out_list.append(int(i[5]))
+    in_list_sum = 0
+    out_list_sum = 0
+    for i in in_list:
+        in_list_sum += i
+    for i in out_list:
+        out_list_sum += i
+    income = in_list_sum - out_list_sum
+    avg = income / items
+    return avg
