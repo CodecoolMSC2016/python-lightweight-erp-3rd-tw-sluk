f8832cc745cb138943f36b62a1e00e13e794b1e4
Nikolett Uracs <loveninagold@gmail.com>
2016-11-30 10:22:16 +0100

    95%

--- accounting/accounting.py
+++ accounting/accounting.py
@@ -31,30 +31,36 @@ def start_module():
                      "Remove",
                      "Update",
                      "Highest profit",
-                     "Avg profit"]
+                     "Avg profit",
+                     "Pringting out"]
 
-    table_accounting = []
-    with open("accounting/items.csv", "r") as my_file:
-        for lines in my_file:
-            table_accounting.append(lines.strip().split(";"))
+    table = data_manager.get_table_from_file("accounting/items.csv")
 
-    ui.print_menu("Accounting Manager", account_table, "Back to main menu")
-    inputs = ui.get_inputs(["Please enter a number: "], "")
-    option = inputs[0]
-    if option == "1":
-        add(table_accounting)
-    elif option == "2":
-        remove(table_accounting)
-    elif option == "3":
-        update(table_accounting)
-    elif option == "4":
-        which_year_max(table_accounting)
-    elif option == "5":
-        avg_amount(table_accounting)
-    elif option == "0":
-        sys.exit(0)
-    else:
-        raise KeyError("There is no such option.")
+    while True:
+        ui.print_menu("Accounting Manager", account_table, "Back to main menu")
+        inputs = ui.get_inputs(["Please enter a number: "], "")
+        option = inputs[0]
+        if option == "1":
+            add(table)
+        elif option == "2":
+            ID = ui.get_inputs(["Give an ID: "], "")
+            remove(table, ID[0])
+        elif option == "3":
+            ID = ui.get_inputs(["Give an ID: "], "")
+            update(table, ID[0])
+        elif option == "4":
+            print(ui.print_result(which_year_max(table), ''), " has the highest profit")
+            ui.get_inputs(["Press any key to continue..."], "")
+        elif option == "5":
+            avg_year = ui.get_inputs(["Give a year, you want to check:"], "")
+            result = avg_amount(table, avg_year[0])
+            print("The Average profit in ", int(avg_year[0]), "was", ui.print_result(result, ''))
+            ui.get_inputs(["Press any key to continue..."], "")
+        elif option == "6":
+            show_table(table)
+        elif option == "0":
+            break
+
     pass
 
 
@@ -62,7 +68,6 @@ def start_module():
 #
 # @table: list of lists
 def show_table(table):
-    print(table)
     ui.print_table(table, ["ID", "Month", "Day", "Year", "In/out", "Amount"])
 
 
@@ -78,6 +83,7 @@ def add(table):
                             "In or out(come): ", "Amount: " ], "Adding record")
 
     table.append([new_id[0], new_id[1], new_id[2], new_id[3], new_id[4], new_id[5]])
+    data_manager.write_table_to_file('accounting/items.csv', table)
     return table
 
 
@@ -87,10 +93,13 @@ def add(table):
 # @id_: string
 def remove(table, id_):
 
-    for i in range(len(table)):
-        if id_ in table[i]:
-            table.remove(table[i])
-
+    for i in table:
+        if id_ in i[0]:
+            table.remove(i)
+            print("Item successfully removed!")
+        else:
+            print("ID not found!")
+    data_manager.write_table_to_file('accounting/items.csv', table)
     return table
 
 
@@ -101,16 +110,34 @@ def remove(table, id_):
 # @id_: string
 def update(table, id_):
 
-    for i in range(len(table)):
-        if id_ in table[i]:
-            update_id = ui.get.inputs(["Give a month: ", "Give a day: ", "Give a year: ",
-                                    "In or out(come): ", "Amount: " ], "Adding record")
-            table[i][1] = update_id[0]
-            table[i][2] = update_id[1]
-            table[i][3] = update_id[2]
-            table[i][4] = update_id[3]
-            table[i][5] = update_id[4]
-
+    for i in table:
+        if id_ in i[0]:
+            update_table = ["Month",
+                             "Day",
+                             "Year",
+                             "In/Out",
+                             "Amount"]
+            ui.print_menu("What do you want to change?", update_table, "Back to accounting menu")
+            inputs = ui.get_inputs(["Please enter a number: "], "")
+            option = inputs[0]
+            if option == "1":
+                updating = ui.get_inputs(["Write in the record:"], "")
+                i[1] = updating[0]
+            elif option == "2":
+                updating = ui.get_inputs(["Write in the record:"], "")
+                i[2] = updating[0]
+            elif option == "3":
+                updating = ui.get_inputs(["Write in the record:"], "")
+                i[3] = updating[0]
+            elif option == "4":
+                updating = ui.get_inputs(["Write in the record:"], "")
+                i[4] = updating[0]
+            elif option == "5":
+                updating = ui.get_inputs(["Write in the record:"], "")
+                i[5] = updating[0]
+            elif option == "0":
+                break
+    data_manager.write_table_to_file('accounting/items.csv', table)
     return table
 
 
@@ -132,22 +159,18 @@ def which_year_max(table):
 # the question: What is the average (per item) profit in a given year? [(profit)/(items count) ]
 # return the answer (number)
 def avg_amount(table, year):
-    in_list = []
-    out_list = []
-    items = 0
-    for i in table:
-        if int(i[3]) == int(year):
-            items += 1
-            if str(i[4]) == "in":
-                in_list.append(int(i[5]))
-            elif str(i[4]) == "out":
-                out_list.append(int(i[5]))
-    in_list_sum = 0
-    out_list_sum = 0
-    for i in in_list:
-        in_list_sum += i
-    for i in out_list:
-        out_list_sum += i
-    income = in_list_sum - out_list_sum
-    avg = income / items
-    return avg
+    avg = 0
+    counter = 0
+    for item in table:
+        if int(item[3]) == year:
+            if item[4] == 'in':
+                avg += int(item[5])
+            elif item[4] == 'out':
+                avg -= int(item[5])
+            counter += 1
+
+    if counter == 0:
+        result = 0
+    else:
+        result = avg / counter
+    return result
